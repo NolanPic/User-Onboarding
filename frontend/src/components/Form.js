cb7ecae4d80350bc1d2f9ecb0ea0e737627c5e09
Nolan Picini <nolanpic@gmail.com>
2019-12-19 13:25:03 -0800

    Added all needed fields to form

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,7 +1,50 @@
 import React from 'react';
+import { withFormik, Field } from 'formik';
 
 const Form = props => {
-    return (<form>I'm a form</form>);
+    const {
+        values,
+        touched,
+        errors,
+        handleSubmit
+    } = props;
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Name:
+                <Field type="text" name="name" />
+            </label>
+
+            <label>
+                Email:
+                <Field type="email" name="email" />
+            </label>
+
+            <label>
+                Password:
+                <Field type="password" name="password" />
+            </label>
+
+            <label>
+                I agree to the terms and conditions
+                <Field type="checkbox" name="termsAccepted" />
+            </label>
+
+            <button type="submit">Submit</button>
+        </form>
+    );
 };
 
-export default Form;
+export default withFormik({
+    mapPropsToValues: () => ({
+        name: '',
+        email: '',
+        password: '',
+        termsAccepted: false
+    }),
+
+    handleSubmit: (values, { setSubmitting }) => {
+        console.log('values', values);
+    }
+})(Form);
